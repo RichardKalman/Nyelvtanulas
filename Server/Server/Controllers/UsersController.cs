06f7f2c2166150bf5c06a0944466517feaf13793
RIchárd Kálmán <kalmanrichard112096@gmail.com>
2021-12-05 16:27:09 +0100

    Update wip

--- Server/Server/Controllers/UsersController.cs
+++ Server/Server/Controllers/UsersController.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Server.Interfaces;
 using AutoMapper;
 using Server.DTOs;
+using System.Security.Claims;
 
 namespace Server.Controllers
 {
@@ -26,6 +27,8 @@ namespace Server.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
+            
+
             var users = await _userRepository.GetMembersAsync();
 
             return Ok(users);
@@ -36,6 +39,21 @@ namespace Server.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
---
+++ Server/Server/DTOs/MemberUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Server.DTOs
+{
+    public class MemberUpdateDto
+    {
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
